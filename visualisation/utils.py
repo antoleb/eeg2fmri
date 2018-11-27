dc2631a5086fa41d202bf6e350e3d985315a67e1
antoleb <anleb96@gmail.com>
2018-11-27 14:52:01 +0300

    visual fix

--- visualisation/utils.py
+++ visualisation/utils.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def concatenate_collage(tensor):
     col_list = [np.concatenate([
-        tensor[..., i] for i in range(start_i, start_i + 5)
+        tensor[:, :, i] for i in range(start_i, start_i + 5)
     ], 0) for start_i in range(0, 30, 5)]
     collage = np.concatenate(col_list, 1)
     return collage
